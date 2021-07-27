4d27911379d31a16c05e49fde45a0552487d4114
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-07-27 12:52:36 +0300

    ch04 executing a method when a bean is created

---
+++ src/main/java/com/muhammedtopgul/ch04/beanCreation/executingMethod/Singer.java
@@ -0,0 +1,63 @@
+package com.muhammedtopgul.ch04.beanCreation.executingMethod;
+
+/*
+ * created by Muhammed Topgul
+ * on 27/07/2021
+ * at 11:40
+ */
+
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Singer {
+
+    private static final String DEFAULT_NAME = "Eric Clapton";
+    private String name;
+    private int age = Integer.MIN_VALUE;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void init() {
+        System.out.println("Initializing bean");
+        if (name == null) {
+            System.out.println(":::Using default name");
+            name = DEFAULT_NAME;
+        }
+        if (age == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException(
+                    "You must set the age property of any beans of type " + Singer.class);
+        }
+    }
+
+    public String toString() {
+        return "\tName: " + name + "\n\tAge: " + age;
+    }
+
+    public static void main(String... args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("ch04/beanCreation/executingMethod/beans.xml");
+        ctx.refresh();
+        getBean("singerOne", ctx);
+        getBean("singerTwo", ctx);
+        getBean("singerThree", ctx);
+    }
+
+    public static Singer getBean(String beanName, ApplicationContext ctx) {
+        try {
+            Singer bean = (Singer) ctx.getBean(beanName);
+            System.out.println(bean);
+            return bean;
+        } catch (BeanCreationException ex) {
+            System.out.println("An error occured in bean configuration: "
+                    + ex.getMessage());
+            return null;
+        }
+    }
+}
