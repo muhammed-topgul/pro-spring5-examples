468ddac972d1c3472bc220b01a2b5b7e30ec6e7b
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-07-27 16:37:43 +0300

    ch04 using the BeanNameAware interface

---
+++ src/main/java/com/muhammedtopgul/ch04/springAware/beanNameAwareInterface/NamedSingerDemo.java
@@ -0,0 +1,21 @@
+package com.muhammedtopgul.ch04.springAware.beanNameAwareInterface;
+
+/*
+ * created by Muhammed Topgul
+ * on 27/07/2021
+ * at 16:29
+ */
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class NamedSingerDemo {
+
+    public static void main(String... args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("ch04/springAware/beanNameAwareInterface/beans.xml");
+        ctx.refresh();
+        NamedSinger bean = (NamedSinger) ctx.getBean("namedSinger");
+        bean.sing();
+        ctx.close();
+    }
+}
