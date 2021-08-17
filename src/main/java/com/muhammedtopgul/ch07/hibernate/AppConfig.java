35dd60a35e1cf225ac7e0aef6d20933e8dd7945b
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-08-17 17:28:30 +0300

    ch07 hibernate configured in spring

---
+++ src/main/java/com/muhammedtopgul/ch07/hibernate/AppConfig.java
@@ -0,0 +1,73 @@
+package com.muhammedtopgul.ch07.hibernate;
+
+/*
+ * created by Muhammed Topgul
+ * on 17/08/2021
+ * at 17:10
+ */
+
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "com.muhammedtopgul.ch07")
+@EnableTransactionManagement
+public class AppConfig {
+
+    private static Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+    @Bean
+    public DataSource dataSource() {
+        try {
+            EmbeddedDatabaseBuilder databaseBuilder = new EmbeddedDatabaseBuilder();
+            return databaseBuilder
+                    .setType(EmbeddedDatabaseType.H2)
+                    .addScripts("ch07/schema.sql", "ch07/test-data.sql")
+                    .build();
+        } catch (Exception exception) {
+            logger.error("Embedded DataSource bean cannot be created!", exception);
+            return null;
+        }
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect","org.hibernate.dialect.H2Dialect");
+        hibernateProperties.put("hibernate.format_sql", true);
+        hibernateProperties.put("hibernate.use_sql_comments", true);
+        hibernateProperties.put("hibernate.show_sql", true);
+        hibernateProperties.put("hibernate.max_fetch_dept", 3);
+        hibernateProperties.put("hibernate.jdbc.batch_size", 10);
+        hibernateProperties.put("hibernate.jdbc.fetch_size", 50);
+        return hibernateProperties;
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() throws IOException {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("com.muhammedtopgul.ch07.entity");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws IOException {
+        return new HibernateTransactionManager(sessionFactory());
+    }
+}
